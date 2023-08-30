a2e9c8a495441d292e09efa9312fcc38415e593d
Chotirose Sukkapat <chotirosemook@gmail.com>
2023-08-30 16:14:15 +0700

    Update __init__.py

--- src/AscendQaCommonLibrary/__init__.py
+++ src/AscendQaCommonLibrary/__init__.py
@@ -6,7 +6,7 @@ from .utils.JsonUtils import JsonUtils
 from .utils.AdbUtils import AdbUtils
 from .utils.AppiumUtils import AppiumUtils
 from robot.api.deco import keyword,not_keyword,library
-from .keywords import common_keyword
+from .keywords.common_keyword import common_keyword
 
 __version__ = VERSION
 

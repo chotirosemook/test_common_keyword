eab8808a76339f0b6ac24f836c8433964e3800b7
Chotirose Sukkapat <chotirosemook@gmail.com>
2023-08-30 18:02:03 +0700

    Update __init__.py

--- src/AscendQaCommonLibrary/__init__.py
+++ src/AscendQaCommonLibrary/__init__.py
@@ -11,7 +11,7 @@ from .keywords.common_keyword import common_keyword
 __version__ = VERSION
 
 @library
-class AscendQaCommonLibrary(object)
+class AscendQaCommonLibrary(object):
 # class AscendQaCommonLibrary(common_keyword,DateUtils,GeneralUtils,ImageUtils,JsonUtils,AdbUtils,AppiumUtils):
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
     ROBOT_LIBRARY_VERSION = __version__
